ffeab33bc2a0d2c7ad6c77ffe645ec216cb9d464
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-04-29 16:58:30 +0530

    lint fix

--- blocks/form/components/checkbox-with-others/checkbox-with-others.js
+++ blocks/form/components/checkbox-with-others/checkbox-with-others.js
@@ -1,15 +1,16 @@
 import { subscribe } from '../../rules/index.js';
 
 export default function decorate(fieldDiv, fieldJson, container, formId) {
-  const otherOptionLabel = fieldJson?.properties?.otherOptionLabel || 'Other';
   const otherOptionLabels = ['other', 'Other'];
   subscribe(fieldDiv, formId, async (element, fieldModel) => {
     const otherInput = fieldDiv.querySelector('.text-wrapper input[name*="othertextinput"]');
     if (otherInput) {
       otherInput.addEventListener('blur', (e) => {
-        const checkboxField = fieldModel._children.find(child => child.fieldType === 'checkbox-group');
+        // eslint-disable-next-line no-underscore-dangle
+        const checkboxField = fieldModel._children.find((child) => child.fieldType === 'checkbox-group');
         if (checkboxField) {
-          const otherIndex = checkboxField.enumNames.findIndex(name => otherOptionLabels.includes(name));
+          const otherIndex = checkboxField.enumNames
+            .findIndex((name) => otherOptionLabels.includes(name));
           if (otherIndex !== -1) {
             const oldValue = checkboxField.enum[otherIndex];
             checkboxField.enum[otherIndex] = e.target.value;
