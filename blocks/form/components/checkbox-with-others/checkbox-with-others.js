74e568a2d0b9c2c6e81a9550c57e6399c853c4ea
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-04-29 16:45:47 +0530

    final fix

--- blocks/form/components/checkbox-with-others/checkbox-with-others.js
+++ blocks/form/components/checkbox-with-others/checkbox-with-others.js
@@ -11,7 +11,12 @@ export default function decorate(fieldDiv, fieldJson, container, formId) {
         if (checkboxField) {
           const otherIndex = checkboxField.enumNames.findIndex(name => otherOptionLabels.includes(name));
           if (otherIndex !== -1) {
+            const oldValue = checkboxField.enum[otherIndex];
             checkboxField.enum[otherIndex] = e.target.value;
+            const idx = checkboxField.value.indexOf(oldValue);
+            if (idx !== -1) {
+              checkboxField.value[idx] = e.target.value;
+            }
           }
         }
       });
